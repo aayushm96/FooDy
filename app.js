25888353ca30386d6433dd97523a04d06e5e522e
aayushm96 <aayushm96@yahoo.com>
2021-08-31 21:23:01 +0530

    formatted the doc

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
-const loginradio=document.querySelector('#login');
-const signupradio=document.querySelector('#signup');
+const loginradio = document.querySelector('#login');
+const signupradio = document.querySelector('#signup');
 
-const box=document.querySelector('.tab-box');
+const box = document.querySelector('.tab-box');
 
 
-loginradio.addEventListener('click' , showL);
-signupradio.addEventListener('click' ,showS)
+loginradio.addEventListener('click', showL);
+signupradio.addEventListener('click', showS)
 
-function showL(e){
+function showL(e) {
     e.preventDefault();
-    box.children[0].style.visibility='visible';
-    box.children[1].style.visibility='hidden';
+    box.children[0].style.visibility = 'visible';
+    box.children[1].style.visibility = 'hidden';
     // console.log(box.children[1]);
 }
-function showS(e){
+function showS(e) {
     e.preventDefault();
-    box.children[0].style.visibility='hidden';
-    box.children[1].style.visibility='visible';
+    box.children[0].style.visibility = 'hidden';
+    box.children[1].style.visibility = 'visible';
     // console.log(box.children[1]);
 }
